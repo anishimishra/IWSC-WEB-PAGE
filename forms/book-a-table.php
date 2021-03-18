bee4985280282ba7de33c9cb7f1ba9fb83e0f498
Anishi Mishra <55035814+anishimishra@users.noreply.github.com>
2021-03-18 13:05:08 +0530

    .

--- forms/book-a-table.php
+++ forms/book-a-table.php
@@ -21,7 +21,7 @@
   $book_a_table->to = $receiving_email_address;
   $book_a_table->from_name = $_POST['name'];
   $book_a_table->from_email = $_POST['email'];
-  $book_a_table->subject = "New table booking request from the website";
+  $book_a_table->subject = "New query from the website";
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
@@ -36,9 +36,6 @@
   $book_a_table->add_message( $_POST['name'], 'Name');
   $book_a_table->add_message( $_POST['email'], 'Email');
   $book_a_table->add_message( $_POST['phone'], 'Phone', 4);
-  $book_a_table->add_message( $_POST['date'], 'Date', 4);
-  $book_a_table->add_message( $_POST['time'], 'Time', 4);
-  $book_a_table->add_message( $_POST['people'], '# of people', 1);
   $book_a_table->add_message( $_POST['message'], 'Message');
 
   echo $book_a_table->send();
